eec4595b5fb4c1782fde8ca72f4a674f2c777a2e
Avery <adsc7378@colorado.edu>
2018-03-10 17:05:22 -0700

    hw3 for current directory

---
+++ hw3.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+
+var fail = function(err) { console.log(err)};
+
+var report = function(size) {
+  console.log("Total size: ", size);
+}
+
+var get_files = function(dir){
+  return new Promise(function(resolve, reject) {
+    fs.readdir(dir, function(err, files) {
+      if (err) { return reject(err)};
+      resolve(files);
+    });
+  });
+}
+
+var get_size = name => {
+  return new Promise(function(resolve, reject) {
+    fs.stat(name, function(err, stats) {
+      if (err) reject(err);
+      resolve(stats.size);
+    });
+  });
+}
+
+var find_type = function(name) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(name, function(err, stats) {
+      if (err) { return reject(err) };
+      resolve([name, stats.isFile(), stats.isDirectory()]);
+    });
+  });
+}
+
+var gather = function(files){
+  var promises = [];
+  files.forEach( info => {
+    var name = info[0];
+    var isFile = info[1];
+    var isDirectory = info[2];
+    if (isFile) {
+      promises.push(get_size(name));
+    }
+    if (isDirectory){
+      promises.push(Promise.resolve(0));
+    }
+  });
+  return Promise.all(promises);
+}
+
+var detect = function(files) {
+  var promises = [];
+  files.forEach(file => promises.push(find_type(file)));
+  return Promise.all(promises);
+}
+
+var sum = function(sizes) {
+  return sizes.reduce((a,b) => a+b);
+}
+
+var readDir = function(dir){
+  var files = get_files(dir);
+  return new Promise(function(resolve, reject) {
+    var size = files.then(detect).then(gather).then(sum).catch(fail);
+    resolve(size);
+  });
+}
+
+var report = function(dir){
+  var size = readDir(dir);
+  size.then(total=>console.log("Total size: ", total));
+}
+
+report('.');
